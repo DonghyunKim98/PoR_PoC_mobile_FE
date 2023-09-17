45c8e9e3b6cae2a612f676bf5df8939b04f5f4ea
DongHyunKim <akainoo123@gmail.com>
2023-09-17 17:21:45 +0900

    feat : add asset list asset item

--- src/screens/asset-list/asset-list.screen.tsx
+++ src/screens/asset-list/asset-list.screen.tsx
@@ -1,14 +1,15 @@
-import { Box, Column, Columns, Stack } from '@mobily/stacks';
+import { Box } from '@mobily/stacks';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useTranslation } from 'react-i18next';
-import { FlatList, Image, Pressable } from 'react-native';
+import { FlatList } from 'react-native';
 
 import { RootStackParamList } from '../root.navigator';
 
-import { Header, Text } from '@/atoms';
+import { AssetListAssetItem } from './components';
+
+import { Header } from '@/atoms';
 import { BasicLayout } from '@/layouts';
-import { palette } from '@/utils';
 
 type AssetListScreenProps = {};
 
@@ -53,91 +54,7 @@ export const AssetListScreen = ({}: AssetListScreenProps) => {
         <FlatList
           data={tempData}
           renderItem={({ item }) => {
-            const { name, logoUrl, unit, myAsset } = item;
-
-            return (
-              <Pressable>
-                {({ pressed }) => {
-                  return (
-                    <Box
-                      style={{
-                        borderColor: palette['primary'],
-                        borderWidth: 0.5,
-                        borderRadius: 5,
-                      }}>
-                      <Columns
-                        paddingX={12}
-                        paddingY={16}
-                        style={{
-                          borderColor: pressed
-                            ? palette['primary']
-                            : palette['white'],
-                          borderWidth: 1,
-                          borderRadius: 5,
-                        }}>
-                        <Column width="fluid">
-                          <Stack horizontal space={8}>
-                            <Image
-                              source={{ uri: logoUrl }}
-                              style={{ width: 30, height: 30 }}
-                              resizeMode="contain"
-                            />
-                            <Stack space={12}>
-                              <Text
-                                style={{ marginTop: 5 }}
-                                fontWeight="500"
-                                fontSize="20"
-                                lineHeight={20}
-                                color="primary">
-                                {name}
-                              </Text>
-                              <Stack space={6} horizontal>
-                                <Text
-                                  fontWeight="500"
-                                  fontSize="20"
-                                  lineHeight={20}
-                                  color="gray-900">
-                                  {myAsset}
-                                </Text>
-                                <Text
-                                  fontWeight="400"
-                                  fontSize="12"
-                                  lineHeight={20}
-                                  color="gray-300">
-                                  {unit}
-                                </Text>
-                              </Stack>
-                            </Stack>
-                          </Stack>
-                        </Column>
-                        <Column width="content">
-                          <Box
-                            paddingY={5}
-                            alignX="center"
-                            style={{
-                              width: 100,
-                              borderColor: palette['primary'],
-                              borderWidth: 0.5,
-                              borderRadius: 50,
-                              ...(pressed && {
-                                backgroundColor: palette['primary'],
-                              }),
-                            }}>
-                            <Text
-                              fontWeight="600"
-                              fontSize="14"
-                              lineHeight={20}
-                              color={pressed ? 'white' : 'primary'}>
-                              더보기
-                            </Text>
-                          </Box>
-                        </Column>
-                      </Columns>
-                    </Box>
-                  );
-                }}
-              </Pressable>
-            );
+            return <AssetListAssetItem {...item} />;
           }}
           ItemSeparatorComponent={() => <Box style={{ height: 10 }} />}
         />
