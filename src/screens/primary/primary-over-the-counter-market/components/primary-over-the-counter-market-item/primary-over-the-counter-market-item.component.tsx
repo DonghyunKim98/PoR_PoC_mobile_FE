da289cfad174c989b5bda31a8a9666b02e0da1a1
DongHyunKim <akainoo123@gmail.com>
2023-10-07 17:23:49 +0900

    feat : 토큰 증권 장외 거래소 아이템 1차 퍼블리싱

--- src/screens/primary/primary-over-the-counter-market/components/primary-over-the-counter-market-item/primary-over-the-counter-market-item.component.tsx
+++ src/screens/primary/primary-over-the-counter-market/components/primary-over-the-counter-market-item/primary-over-the-counter-market-item.component.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@mobily/stacks';
+import { Box, Stack } from '@mobily/stacks';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -11,9 +11,34 @@ import {
 
 import { Text } from '@/atoms';
 import { getReadAssetsResponseData } from '@/hooks';
+import { palette } from '@/utils';
 
 type PrimaryOverTheCounterMarketItemProps =
   getReadAssetsResponseData['data'][0];
+
+const ItemPriceContent = ({
+  title,
+  num,
+  unit,
+}: {
+  title: string;
+  num: string;
+  unit: string;
+}) => {
+  return (
+    <Stack horizontal space={6} align="center">
+      <Text fontSize="16" fontWeight="300" lineHeight={20} color="primary">
+        {title}
+      </Text>
+      <Text fontSize="16" fontWeight="600" lineHeight={20} color="primary">
+        {num}
+        <Text fontSize="12" fontWeight="300" lineHeight={20} color="gray-700">
+          {'  ' + unit}
+        </Text>
+      </Text>
+    </Stack>
+  );
+};
 
 export const PrimaryOverTheCounterMarketItem =
   memo<PrimaryOverTheCounterMarketItemProps>(
@@ -27,29 +52,112 @@ export const PrimaryOverTheCounterMarketItem =
       const { t } = useTranslation();
 
       return (
-        <Stack>
-          <Stack horizontal space={10}>
+        <Box
+          style={{
+            borderRadius: 5,
+            borderColor: palette['primary'],
+            borderWidth: 0.5,
+          }}>
+          <Stack
+            horizontal
+            space={10}
+            paddingX={10}
+            paddingY={20}
+            style={{
+              borderBottomWidth: 0.5,
+              borderBottomColor: palette['primary'],
+            }}>
             <Image
               source={{ uri: logoUrl }}
               style={{ width: 30, height: 30 }}
               resizeMode="contain"
             />
-            <Stack space={18}>
+            <Stack space={18} marginTop={5}>
               <Text
                 fontWeight="700"
-                fontSize="20"
-                lineHeight={20}
+                fontSize="18"
+                lineHeight={18}
                 color="primary">
                 {t(`SingleAsset_id${assetId}_name`)}
                 <Text
                   fontWeight="500"
-                  fontSize="20"
-                  lineHeight={20}
-                  color="primary">{`(투자계약 증권 ${assetId}호)`}</Text>
+                  fontSize="18"
+                  lineHeight={18}
+                  color="primary">
+                  {`(투자계약 증권 ${assetId}호)`}
+                </Text>
               </Text>
+              <Stack space={4}>
+                <ItemPriceContent title="발행량" num={balance} unit={unit} />
+                <ItemPriceContent
+                  title="현재가"
+                  num={price.toString()}
+                  unit={'₩'}
+                />
+              </Stack>
             </Stack>
           </Stack>
-        </Stack>
+          <Box
+            direction="row"
+            alignX="between"
+            alignY="center"
+            paddingX={12}
+            paddingY={12}>
+            <ItemPriceContent title="내 자산" num={myAsset} unit={unit} />
+            <Box
+              paddingX={16}
+              paddingY={3}
+              style={{
+                borderRadius: 50,
+                borderColor: palette['primary'],
+                borderWidth: 0.5,
+              }}>
+              <Text
+                fontWeight="600"
+                fontSize="14"
+                lineHeight={20}
+                color="primary">
+                데이터 확인
+              </Text>
+            </Box>
+          </Box>
+          <Box direction="row">
+            <Box
+              alignX="center"
+              flex="fluid"
+              paddingY={14}
+              style={{
+                borderBottomLeftRadius: 5,
+                borderRightWidth: 1,
+                borderRightColor: palette['white'],
+                backgroundColor: palette['gray-700'],
+              }}>
+              <Text
+                fontSize="14"
+                fontWeight="600"
+                color="white"
+                lineHeight={20}>
+                매도
+              </Text>
+            </Box>
+            <Box
+              alignX="center"
+              flex="fluid"
+              paddingY={14}
+              style={{
+                borderBottomRightRadius: 5,
+                backgroundColor: palette['primary'],
+              }}>
+              <Text
+                fontSize="14"
+                fontWeight="600"
+                color="white"
+                lineHeight={20}>
+                매수
+              </Text>
+            </Box>
+          </Box>
+        </Box>
       );
     },
   );
