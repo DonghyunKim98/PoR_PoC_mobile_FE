9dc07b0aee35df5b9a5acc1dfbea10fe879d52ab
DongHyunKim <akainoo123@gmail.com>
2023-10-10 00:38:16 +0900

    feat : asset-sell screen 완료

--- src/screens/asset-sell/asset-sell.screen.tsx
+++ src/screens/asset-sell/asset-sell.screen.tsx
@@ -1,9 +1,18 @@
-import { RouteProp } from '@react-navigation/native';
+import { RouteProp, useRoute } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import isUndefined from 'lodash/isUndefined';
+import { FormProvider } from 'react-hook-form';
+import { useRecoilValue } from 'recoil';
 
 import { RootStackParamList } from '../root.navigator';
 
+import { AssetSellInput, AssetSellSubmit } from './components';
+import { useAssetSellForm } from './hooks';
+
 import { Header } from '@/atoms';
+import { useGetReadAssetsQuery } from '@/hooks';
+import { LoadingPage, ScrollView } from '@/layouts';
+import { $userKeyState } from '@/states';
 
 type AssetSellScreenProps = {};
 
@@ -18,14 +27,44 @@ export type AssetSellScreenNavigationRouteProps = RouteProp<
 >;
 
 export const AssetSellScreen = ({}: AssetSellScreenProps) => {
+  const { key } = useRecoilValue($userKeyState);
+
+  const {
+    params: { assetId },
+  } = useRoute<AssetSellScreenNavigationRouteProps>();
+
+  const { isLoading, data } = useGetReadAssetsQuery({ key });
+  const methods = useAssetSellForm();
+
+  if (isLoading || isUndefined(data)) {
+    return <LoadingPage />;
+  }
+
+  const currentAssetData = data.data.find(asset => asset.assetId === assetId);
+
+  if (isUndefined(currentAssetData)) {
+    return null;
+  }
+
+  const { logoUrl, price, myAsset } = currentAssetData;
+
   return (
-    <>
-      <Header
-        title="토큰 증권 장외 거래소 (매도)"
-        titleColor="primary"
-        leftIconColor="primary"
-        backgroundColor="white"
-      />
-    </>
+    <FormProvider {...methods}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Header
+          title="토큰 증권 장외 거래소 (매도)"
+          titleColor="primary"
+          leftIconColor="primary"
+          backgroundColor="white"
+        />
+        <AssetSellInput
+          logoUrl={logoUrl}
+          price={price}
+          maxAmount={parseInt(myAsset)}
+          assetId={assetId}
+        />
+        <AssetSellSubmit />
+      </ScrollView>
+    </FormProvider>
   );
 };
